cada392d28ea3359d42c0956d7ddc8af0505cafe
ankit.gupta20 <ankit.gupta@appventurez.com>
2022-04-06 16:39:57 +0530

    update settings.py

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -4,10 +4,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-try:
-    from StringIO import StringIO ## for Python 2
-except ImportError:
-    from io import StringIO ## for Python 3
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
